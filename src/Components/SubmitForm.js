67bd8f57b65a912a1e257911e0aa557dc3668da4
Shefali Kashyap <kshyapshef@outlook.com>
2023-02-15 16:06:16 +0530

    test
    test

--- src/Components/SubmitForm.js
+++ src/Components/SubmitForm.js
@@ -7,6 +7,7 @@ export default function SubmitForm() {
         <input class="form" type="search" placeholder="Search" aria-label="Search" />
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit" >Search</button>
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign In</button>
+        abcc
       </form>
     </div>
   )
